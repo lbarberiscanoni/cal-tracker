5ff04af7a41505340c6994befc61186e193a6c84
lbarberiscanoni <hllbck7@gmail.com>
2025-02-15 20:14:28 -0800

    initial transfer of the app

---
+++ pages/api/index.jsx
@@ -0,0 +1,13 @@
+// pages/index.jsx
+import React from 'react';
+import Cal from '../components/Cal';
+
+const Home = () => {
+  return (
+    <div>
+      <Cal />
+    </div>
+  );
+};
+
+export default Home;
